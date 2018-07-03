81e47351cda69a33d340137ff9c661dd3f011b1c
Bruno Zani <ubzani@gmail.com>
2018-07-03 00:47:09 -0300

    Tela Inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
 @IonicPage()
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
