d9a2a1057f2ed5b51120e81601cb1373a442d68c
bzani <ubzani@gmail.com>
2018-07-25 02:05:42 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -27,14 +27,59 @@ export class CartService {
 
     // adicionar produto ao carrinho
     addProduto(produto: ProdutoDTO) : Cart {
-        let cart = this.getCart();                      // verifica se produto ja existe
+        let cart = this.getCart();
         let position = cart.items.findIndex(x => x.produto.id == produto.id);
-        if(position == -1) {                            // produto nao existe ainda
+        if(position == -1) {  // produto nao existe ainda
             cart.items.push({quantidade: 1, produto: produto});
         }
-        this.storage.setLocalCart(cart);                // atualiza carrinho na localStorage
+        this.storage.setLocalCart(cart);  // atualiza carrinho na localStorage
         return cart;
     }
 
+    // remover produto do carrinho
+    removeProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1) {  // encontrou produto
+            cart.items.splice(position, 1);
+        }
+        this.storage.setLocalCart(cart);  // atualiza carrinho na localStorage
+        return cart;
+    }
+
+    // incrementar qtde do produto no carrinho
+    increaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1) {  // encontrou produto
+            cart.items[position].quantidade++;
+        }
+        this.storage.setLocalCart(cart);  // atualiza carrinho na localStorage
+        return cart;
+    }
+
+    // decrementar qtde do produto no carrinho
+    decreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1) {  // encontrou produto
+            cart.items[position].quantidade--;
+            if (cart.items[position].quantidade < 1) {
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setLocalCart(cart);  // atualiza carrinho na localStorage
+        return cart;
+    }
+
+    // calcular valor total do carrinho
+    total() : number {
+        let cart = this.getCart();
+        let sum = 0;
+        for (var i=0; i<cart.items.length; i++) {
+            sum += cart.items[i].produto.preco * cart.items[i].quantidade;
+        }
+        return sum;
+    }
 
 }
