588eeedaafbe1e4c3068af548354a969073d9299
bzani <ubzani@gmail.com>
2018-07-30 23:42:06 -0300

    Instalando e testandoo apk no dispositivo

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -61,16 +61,33 @@ export class ProfilePage {
   getCameraPicture() {
     this.cameraOn = true;
     const options: CameraOptions = {
-      quality: 100,  // maxima
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
     
     this.camera.getPicture(options).then((imageData) => {
-     this.picture = 'data:image/png;base64,' + imageData;
-     this.cameraOn = false;
-    }, (err) => {});
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
+  getGalleryPicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+    }, (err) => {
+    });
   }
 
   sendPicture() {
